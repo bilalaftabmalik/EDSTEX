e6877762219351b1dc3f5d11f49e16cd6254aee2
ibilalbashir <ibilalbashir@gmail.com>
2019-01-29 19:20:53 +0500

    product page

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductContainerComponent } from './product-container/product-container.component';
+import { GalleryComponent } from './product-container/gallery/gallery.component';
+import { FormComponent } from './product-container/form/form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import { SuggestionsComponent } from './product-container/suggestions/suggestions.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    ProductRoutingModule
+    ProductRoutingModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule
   ],
-  declarations: [ProductContainerComponent]
+  declarations: [ProductContainerComponent, GalleryComponent, FormComponent, SuggestionsComponent]
 })
 export class ProductModule { }
